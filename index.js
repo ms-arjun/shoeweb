1b4e76fb595c6856e70d5865835fa30524869510
ms-arjun <arjunmurugesan11@gmail.com>
2024-03-13 14:54:17 +0530

    last push

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#welcome").click(function(){
+      $("#welcome1").slideToggle("slow");
+    });
+  });
+  
+  function btn1(){
+    document.getElementById("main").src="./assets/img2.jfif";
+  }
+
+  function btn2(){
+    document.getElementById("main").src=src="./assets/img3.jfif";
+  }
+  function btn3(){
+    document.getElementById("main").src=src="./assets/img5.jfif";
+  }
+
+  function btn4(){
+    document.getElementById("main").src="./assets/img7.jfif";
+  }
+  function btn5(){
+    document.getElementById("main").src="./assets/img10.jfif";
+  }
+  function btn6(){
+    document.getElementById("main").src="./assets/img9.jfif";
+  }
